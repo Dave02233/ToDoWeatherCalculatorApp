03c764645d6ad3efeea45e6a26c777060031dbee
Dave02233 <davidecannistraro02@gmail.com>
2025-07-10 01:19:59 +0200

    Calcolatrice brutta ma funzionante

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -3,29 +3,55 @@ import { useState, useEffect } from 'react';
 
 function Calculator ({ toggleVisibility }) {
     
-    const [acc1, setAcc1] = useState(0);
+    const [acc1, setAcc1] = useState([]);
     const [acc2, setAcc2] = useState(0);
-    const [result, setResult] = useState(0);
 
-    useEffect( () => {
-        setResult(acc1 + acc2);
-        console.log('render')
-    }, [acc1])
+    const handleClick = (num) => {
+        if (num === '.' && acc1.includes('.')) return;
+        setAcc1(prev => [...prev, num]);
+    };
 
-    const handleClick1 = () => {
-        setAcc1(prev => {
-            prev.toString() += 1
-        })
+    const handleOperation = (type) => {
+        const value = Number(acc1.join(''));
+        
+        if (!acc2) {
+            setAcc2(value); 
+            setAcc1([]);
+            return;
+        } 
+
+        if (!isNaN(value)) {
+        setAcc2(prev =>
+            type === '+' ? prev + value : 
+            type === '-' ? prev - value : 
+            type === '*' ? prev * value : 
+            type === '/' ? prev / value : prev
+        );
+        setAcc1([]);
+        }
+        
+    };
+
+    const handleCanc = () => {
+        setAcc2(0)
     }
 
 
     return (
-        <>
-            <button onClick={toggleVisibility}>Close</button>
-            <input disabled={true} value={result} />
-            <br />
-            <button onClick={handleClick1}>1</button>
-        </>
+        <div className={styles.calculatorContainer}>
+            <button className={styles.close} onClick={toggleVisibility}>Close</button>
+            <button className={styles.delete} onClick={handleCanc}>Del</button>
+            <input className={styles.acc1} type='text' value={acc2} disabled={true}/>
+            <input type='text' value={acc1.join('')} disabled={true}/>
+            <div className={styles.numbers}>
+                {[1,2,3,4,5,6,7,8,9,0, '.'].map(num => <button onClick={() => handleClick(num)}>{num}</button>)}
+            </div>
+            <div className={styles.operators}>
+                {['+', '-', '*', '/'].map(op => <button onClick={() => handleOperation(op)}>{op}</button>)}
+            </div>
+            
+            
+        </div>
     );
 }
 
@@ -38,13 +64,20 @@ export default function CalculatorContainer () {
     }
 
     return (
-        <div className={styles.calculatorContainer}>
-            {
-                visible 
-                ? <Calculator toggleVisibility={handleClickVisible}/>
-                : <button className={styles.calculatorVisibleButton} onClick={handleClickVisible}>_</button>
-            }
-        </div>
+    <>
+        {
+            !visible ?
+
+            <div className={styles.calculatorIconContainer}>
+                <button className={styles.calculatorVisibleButton} onClick={handleClickVisible}>_</button>
+            </div>
+            
+            : <Calculator toggleVisibility={handleClickVisible}/>
+        }
+        
+
+    </>
+
     )
 }
 

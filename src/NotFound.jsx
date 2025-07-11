b31860f4efa97db57a85694dcf0e9edca0889055
Dave <davidecannistraro02@gmail.com>
2025-07-11 12:37:18 +0200

    Update NotFound.jsx

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 
 export default function NotFound () {
     return (
         <>
             <h1>Error 404: Page Not Found</h1>
-            <Link to='/'>Torna alla Home</Link>
+            <Navigate to='/' />
         </>
     )
 }
